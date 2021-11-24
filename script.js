038795fb34107966731ef28553893e1299b2f6b6
Rafaero <rafaelpaivavitor@hotmail.com>
2021-11-24 12:07:03 -0300

    add card id

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function calc(){
+    let id = document.getElementById("id").value;
     let questionOne = document.getElementById("one").value
     let questionTwo = document.getElementById("two").value
     let questionThree = document.getElementById("three").value
@@ -30,12 +31,54 @@ function calc(){
 
     if(porcetagem >= 0){
         document.getElementById("show-porcent").innerHTML = resultPorcent
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDEzMjIzNzEsImVtYWlsIjoibHV6LmlyaWFydGVAa2V5cnVzLmNvbSIsImFwcGxpY2F0aW9uIjozMDAxMjYwMDl9fQ.vdpen9-ki7OI5k8uTwgnged5Quq8R1woBLjii6Bd0cVVV7lwFg4ZBw2_3kH_7u2zR7bXZ95a_omGsQA9AYR8BQ',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ query: `mutation{
+                    updateCardField(input: {
+                        card_id: "${id}"
+                        field_id: "porcetagem"
+                        new_value: "${resultPorcent}"
+                    }){clientMutationId}}`})
+            };
+        
+            fetch('https://api.pipefy.com/graphql', options)
+                .then(response => response.json())
+                .then(response => console.log(response))
+                .catch(err => console.error(err));
+        
     }else{
         document.getElementById("show-porcent").innerHTML = ""
     }
 
     if(sum >= 0 || sum <= 0){
-        document.getElementById("show-sum").innerHTML = sum.toFixed(2)
+        document.getElementById("show-sum").innerHTML = sum.toFixed(2);
+
+        const options = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDEzMjIzNzEsImVtYWlsIjoibHV6LmlyaWFydGVAa2V5cnVzLmNvbSIsImFwcGxpY2F0aW9uIjozMDAxMjYwMDl9fQ.vdpen9-ki7OI5k8uTwgnged5Quq8R1woBLjii6Bd0cVVV7lwFg4ZBw2_3kH_7u2zR7bXZ95a_omGsQA9AYR8BQ',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query: `mutation{
+                updateCardField(input: {
+                    card_id: "${id}"
+                    field_id: "2_1_nota_final"
+                    new_value: "${sum}"
+                }){clientMutationId}}`})
+        };
+    
+        fetch('https://api.pipefy.com/graphql', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    
     }else{
         alert("Preencha todos os campos")
     }
